1e90ce9f85648a44b765c094228beda2ed2a2101
Jayse Hall <jayse.2002hall@gmail.com>
2024-03-02 10:20:02 -0700

    created new slave and master files

---
+++ code/node_master/Master.ino
@@ -0,0 +1,79 @@
+/*
+ *  This sketch demonstrates how to scan WiFi networks.
+ *  The API is based on the Arduino WiFi Shield library, but has significant changes as newer WiFi functions are supported.
+ *  E.g. the return value of `encryptionType()` different because more modern encryption is supported.
+ */
+#include "WiFi.h"
+
+int rssi[5];
+int thresh;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected.
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    Serial.println("Setup done");
+}
+
+void loop()
+{
+    Serial.println("Scan start");
+
+    // WiFi.scanNetworks will return the number of networks found.
+    int n = WiFi.scanNetworks();
+    Serial.println("Scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        Serial.println("Nr | SSID                             | RSSI | CH | Encryption");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.printf("%2d",i + 1);
+            Serial.print(" | ");
+            Serial.printf("%-32.32s", WiFi.SSID(i).c_str());
+            Serial.print(" | ");
+            Serial.printf("%4d", WiFi.RSSI(i));
+            Serial.print(" | ");
+            Serial.printf("%2d", WiFi.channel(i));
+            Serial.print(" | ");
+            switch (WiFi.SSID(i))
+            {
+            case "node0":
+                Serial.print("found node 0");
+                break;
+            case "node1":
+                Serial.print("found node 1");
+                break;
+            case "node2":
+                Serial.print("found node 2");
+                break;
+            case "node3":
+                Serial.print("found node 3");
+                break;
+            case "node4":
+                Serial.print("found node 4");
+                break;
+            default:
+                Serial.print("I dont recognize " + WiFi.SSID());
+            }
+            Serial.println();
+            delay(10);
+        }
+    }
+    Serial.println("");
+
+    // Delete the scan result to free memory for code below.
+    WiFi.scanDelete();
+
+    // Wait a bit before scanning again.
+    delay(1000);
+}
+
+int average()
